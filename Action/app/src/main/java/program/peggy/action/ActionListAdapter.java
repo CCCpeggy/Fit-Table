f68596686e3c4d57fccb227cfe5563db1990087d
CCC <peggy19925@gmail.com>
2018-07-11 08:46:46 +0800

    Adapter use ViewHolder

--- Action/app/src/main/java/program/peggy/action/ActionListAdapter.java
+++ Action/app/src/main/java/program/peggy/action/ActionListAdapter.java
@@ -1,74 +1,57 @@
 package program.peggy.action;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+
+
 
 public class ActionListAdapter extends SimpleCursorAdapter {
     private Context mContext;
     private Context appContext;
     private int layout;
     private Cursor cr;
-    //private final LayoutInflater inflater;
+    private final LayoutInflater inflater;
+    String[] from;
     int[] to;
 
-
     static class ViewHolder{
-        LinearLayout rlBorder;
-        TextView Name;
+        TextView Text1;
+        TextView Text2;
     }
 
     public ActionListAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
         super(context, layout, c, from, to);
         this.layout = layout;
         this.mContext=context;
-        //this.inflater=LayoutInflater.from(context);
+        this.inflater=LayoutInflater.from(context);
         this.cr=c;
-        Log.v("1243","");
+        this.from=from;
         this.to=to;
     }
 
     @Override
-    public void bindView(View view,Context context,Cursor cursor){
-        super.bindView(view, context, cursor);
-        TextView Name=view.findViewById(to[0]);
-        TextView Part=view.findViewById(to[1]);
-
-        int Name_col =cursor.getColumnIndexOrThrow(ActionClass.ActionName_ColName);
-        int Part_col =cursor.getColumnIndexOrThrow(ActionClass.ActionPart_ColName);
-
-        Log.v("123","");
-        Name.setText(cr.getString(Name_col));
-        Part.setText(MyApplication.getActionPart( cr.getInt(Part_col)));
-    }
-/*
-    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
-        //當ListView被拖拉時會不斷觸發getView，為了避免重複加載必須加上這個判斷
         if (convertView == null) {
             holder = new ViewHolder();
-            convertView = inflater.inflate(R.layout.style_listview, null);
-            holder.Name = (TextView) convertView.findViewById(R.id.tvName);
-
-            holder.rlBorder = (LinearLayout) convertView.findViewById(R.id.llBorder);
+            convertView = inflater.inflate(layout, null);
+            holder.Text1 = (TextView) convertView.findViewById(to[0]);
+            holder.Text2 = (TextView) convertView.findViewById(to[1]);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
-        convertView.setBackgroundResource(R.drawable.default_selector);
+        cr.moveToPosition(position);
+        int Text1_colIndex =cr.getColumnIndexOrThrow(from[0]);
+        int Text2_colIndex =cr.getColumnIndexOrThrow(from[1]);
+        holder.Text1.setText(cr.getString(Text1_colIndex));
+        holder.Text2.setText(MyApplication.getActionPart( cr.getInt(Text2_colIndex)));
         return convertView;
-    }*/
+    }
 
 }
